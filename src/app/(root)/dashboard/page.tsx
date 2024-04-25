98e14c8b73eb703015d61d3da4d7d83be1eaa52c
bertorepo <bert.stark3231@gmail.com>
2024-04-25 20:41:08 +0800

    fix: remove unwanted error in dashboard page

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,31 +1,17 @@
-import RedirectMessage from '@/components/pages/auth/redirect-message';
 import { auth } from '@/auth';
 import paths from '@/lib/paths';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
-interface DashboardPageProps {
-  searchParams: {
-    auth: string;
-  };
-}
-
-const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
+const DashboardPage = async () => {
   const session = await auth();
 
   if (!session) {
     redirect(paths.toLogin());
   }
 
-  const isAuthenticated = Boolean(searchParams.auth);
   return (
     <>
-      {isAuthenticated && (
-        <RedirectMessage
-          type="SUCCESS"
-          message={`Welcome back 🎉 ${session.user.username.toLowerCase()}`}
-        />
-      )}
       <h1>Dashboards</h1>
     </>
   );
